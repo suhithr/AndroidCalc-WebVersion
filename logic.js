91f890a29a7feade6559de19303aa21c3273a4cf
suhithr <suhithrajesh@yahoo.com>
2015-05-29 16:29:02 +0530

    Finished basic simple calc

--- logic.js
+++ logic.js
@@ -1,7 +1,22 @@
-var expn = ""
 function show(it) {
-	var display = document.getElementById('display');
-	expn = expn + it;
-	display.value+=expn;
+	var disp = document.getElementById('displayexpn');
+	disp.value += it;
+
+}
+
+function eql() {
+	var dispex = document.getElementById('displayexpn');
+	var dispans = document.getElementById('displayans');
+	var ans = eval(dispex.value);
+
+	dispans.value = ans;
+}
+
+function del() {
+	var disp = document.getElementById('displayexpn');
+
+	var afterdel = disp.value.slice(0, -1);
+
+	disp.value = afterdel;
 
 }
